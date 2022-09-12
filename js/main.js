32c51560c22796595e7f7b66e27b976222428711
Cameron <cameronpeck317@gmail.com>
2022-09-12 21:51:40 +0000

    put form's input values into object and fix some image bugs from queryselecting the element instead of the entire form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var $journalForm = document.querySelector('form');
 var $photoUrlInput = document.querySelector('#photo-url');
 var $image = document.querySelector('img');
 
@@ -5,4 +6,16 @@ $photoUrlInput.addEventListener('input', handleImageUrl);
 
 function handleImageUrl(event) {
   $image.setAttribute('src', $photoUrlInput.value);
+}
+
+$journalForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var formData = {};
+  formData.title = $journalForm.elements.title.value;
+  formData['photo-url'] = $journalForm.elements['photo-url'].value;
+  formData.notes = $journalForm.elements.notes.value;
+  $journalForm.reset();
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
