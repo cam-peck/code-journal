f9c7f030dc90db9594ed9a5e7dbda2e081edc3ce
Cameron <cameronpeck317@gmail.com>
2022-09-13 17:11:37 +0000

    create function that takes a single entry object and returns a dom tree. Loop through entries and append to page when DOMContentLoaded is fired.

--- js/main.js
+++ js/main.js
@@ -4,19 +4,21 @@ var $image = document.querySelector('img');
 
 $photoUrlInput.addEventListener('input', handleImageUrl);
 
-function handleImageUrl(event) {
+$journalForm.addEventListener('submit', handleSubmit);
+
+window.addEventListener('DOMContentLoaded', renderEntry(data));
+
+function handleImageUrl(event) { // handles input urls from entry form and renders them
   if (isImage($photoUrlInput.value)) {
     $image.setAttribute('src', $photoUrlInput.value);
   }
 }
 
-function isImage(url) {
+function isImage(url) { // checks if an image ends in common extensions, returns T / F
   return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
 }
 
-$journalForm.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event) {
+function handleSubmit(event) { // handles the submit event on the new entry form
   event.preventDefault();
   var formData = {};
   formData.title = $journalForm.elements.title.value;
@@ -27,4 +29,47 @@ function handleSubmit(event) {
   data.entries.unshift(formData);
   $journalForm.reset();
   $image.setAttribute('src', 'images/placeholder-image-square.jpg'); // reset image to default
+}
+
+function renderEntry(entry) {
+  /**
+   * <ul class="row mb-1-rem">
+   *   <li class="column-half">
+   *     <img src="" alt="">
+   *   </li>
+   *   <li class="column-half">
+   *     <h2 class="entry-title"></h2>
+   *     <p class="entry-description">
+   *   </li>
+   * </ul>
+  */
+  var $entryContainer = document.querySelector('.entry-container');
+  for (let i = 0; i < entry.entries.length; i++) {
+    var $entry = document.createElement('ul');
+    $entry.classList = 'row mb-1-rem';
+
+    var $imageLi = document.createElement('li');
+    $imageLi.classList = 'column-half';
+
+    var $imageTag = document.createElement('img');
+    $imageTag.setAttribute('src', entry.entries[i]['photo-url']);
+    $imageTag.setAttribute('alt', `${entry.entries[i].title}-img`);
+
+    var $textLi = document.createElement('li');
+    $textLi.classList = 'column-half';
+
+    var $h2Tag = document.createElement('h2');
+    $h2Tag.classList = 'entry-title';
+    $h2Tag.textContent = entry.entries[i].title;
+
+    var $pTag = document.createElement('p');
+    $pTag.classList = 'entry-description';
+    $pTag.textContent = entry.entries[i].notes;
+
+    $entry.append($imageLi, $textLi);
+    $imageLi.append($imageTag);
+    $textLi.append($h2Tag, $pTag);
+
+    $entryContainer.append($entry);
+  }
 }
